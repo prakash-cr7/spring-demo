51a49945ae21ddb8aad28b5d6dee0c051fb7d626
prakash-cr7 <prakashkumarraj1@gmail.com>
2022-06-13 23:40:18 +0530

    init commit

---
+++ src/main/java/com/example/spring_demo/dao/PersonDao.java
@@ -0,0 +1,26 @@
+package com.example.spring_demo.dao;
+
+import com.example.spring_demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        return insertPerson(UUID.randomUUID(), person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+
+}
